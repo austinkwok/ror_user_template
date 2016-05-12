6daa1df3bb96293354c72ad22ba869f1f2b26162
austinkwok <austinkwok615@gmail.com>
2016-05-11 19:12:35 -0700

    log in/out and sessions for remembering user

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -19,11 +19,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_difference 'User.count', 1 do
       post_via_redirect users_path, user: { first_name:  "Example User",
-      										last_name: "k",
+      										                  last_name: "k",
                                             email: "p@example.com",
                                             password:              "password",
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
